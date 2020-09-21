af3244651e8382a2476070cdfff222b3a738f7d1
hyun <dhtmdgussksm@naver.com>
2020-09-21 21:36:53 +0900

    Update Test4.java

--- chap6/Test4.java
+++ chap6/Test4.java
@@ -6,8 +6,23 @@ public class Test4 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int dogCount = 0;
+		int birdCount = 0;
+		System.out.println("dog bird sum : ");
 		int inputNumber = scan.nextInt();
+		System.out.println("dog bird leg sum : ");
 		int inputLeg = scan.nextInt();
+		for(int x = 0; x<inputNumber+1;x++) {
+			for(int y = 0;y<inputNumber+1;y++) {
+				int sumlegs = 4*x + 2*y;
+				if(sumlegs == inputLeg && x+y == inputNumber) {
+					dogCount = x;
+					birdCount = y;
+				}
+			}
+		}
+		System.out.println("dog : " + dogCount);
+		System.out.println("bird : " + birdCount);
 	}
 
 }
