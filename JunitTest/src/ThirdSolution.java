3776a6b391e3655d3adeb9346cd7fc5965570035
hyun <dhtmdgussksm@naver.com>
2020-10-22 09:19:40 +0900

    Update ThirdSolution.java
    Change some visible value check source

--- JunitTest/src/ThirdSolution.java
+++ JunitTest/src/ThirdSolution.java
@@ -2,59 +2,61 @@ import java.util.*;
 
 public class ThirdSolution{
 	public static int solution(int n) {
-		return toDecimalString(reverseOrder(toThirdinayString(n)));
+		System.out.println("==========TestCase==========");
+		System.out.println("argument input : " + n);
+		long answer = toDecimalString(reverseOrder(toThirdinayString(n)));
+		System.out.println("argument output : " + answer);
+		return (int)answer;
 	}
 	
 	static String toThirdinayString(int n) {
-		
-		Stack<Integer> stack = new Stack<>();
+		System.out.println("toThirdinaryString input : " + n);
+		Stack<Long> stack = new Stack<>();
 		StringBuffer sb = new StringBuffer();
-		int curN = n;
-		final int THIRDINARY = 3;
-		int counter = 1;
+		long curN = n;
+		final long THIRDINARY = 3;
 		Boolean EndCondition = true;
 		while(EndCondition) {
-			stack.push(curN%THIRDINARY);
-			curN /= THIRDINARY;
 			if(curN < THIRDINARY) {
 				stack.push(curN);
-				EndCondition = !EndCondition;
+				break;
 			}
+			stack.push(curN%THIRDINARY);
+			curN /= THIRDINARY;
+			
 		}
 		while(!stack.isEmpty()) {
-			System.out.println(stack.peek());
 			sb.append(stack.pop());
 		}
-		System.out.println(sb.toString());
+		System.out.println("toThirdinaryString input : " + sb.toString());
 		return sb.toString();
 	}
 	
-	static int toDecimalString(String n) {
+	static long toDecimalString(String n) {
+		System.out.println("toDEcimalString input : " + n);
 		Queue<Integer> queue = new LinkedList<>();
 		StringBuffer sb = new StringBuffer();
 		String str = n;
 		long answer = 0;
-		final int THIRDINARY = 3;
-		int target = Integer.parseInt(str);
-		System.out.println("Decimal String convert value : " + target);
-		sb.append(Integer.parseInt(str));
+		final long THIRDINARY = 3;
+		sb.append(Long.parseLong(str));
 		sb.reverse();
 		String sbString = sb.toString();
-		System.out.println(sbString);
-		System.out.println("init answer : " + answer);
 		for(int i = 0; i < sbString.length();i++) {
-			System.out.println((sbString.charAt(i)-'0') * Math.pow(THIRDINARY, i));
 			answer += (sbString.charAt(i)-'0') * Math.pow(THIRDINARY, i);
 		}
-		System.out.println(answer);
-		return (int)answer;
+		System.out.println("toDecimalString output : " + answer);
+		return answer;
 	}
 	
 	static String reverseOrder(String str) {
+		System.out.println("reverseOrder input : " + str);
 		StringBuffer sb = new StringBuffer();
 		for(int i = 0; i<str.length(); i++) {
 			sb.append(str.charAt(i));
-		}		
-		return sb.reverse().toString();
+		}
+		String reverseResult = sb.reverse().toString();
+		System.out.println("reverseOrder output : " + reverseResult);
+		return reverseResult;
 	}
 }
