61d739cc5f3d233c51b834633c85d21700a2fcf0
hyun <dhtmdgussksm@naver.com>
2020-10-26 13:35:59 +0900

    Update FailureSolution.java

--- JunitTest/src/FailureSolution.java
+++ JunitTest/src/FailureSolution.java
@@ -1,10 +1,9 @@
 import java.util.*;
-import java.util.Map.Entry;
 
 class FailureSolution {
 	public static int[] solution(int N, int[] stages) {
 		final int STAGE_MAX = N + 1;
-		int[] answer = {};
+		int[] answer = new int[N];
 		double[] failureRateArray = new double[STAGE_MAX];
 		double servivor = stages.length;
 		for (int i = 0; i < failureRateArray.length; i++) {
@@ -18,54 +17,45 @@ class FailureSolution {
 			servivor -= count;
 		}
 
-		Map<Integer, Double> map = new LinkedHashMap<>();
 		double[] resultArray = new double[N];
+		int[] indexArray = new int[N];
 		for (int i = 0; i < resultArray.length; i++) {
 			resultArray[i] = failureRateArray[i];
 			System.out.println(resultArray[i]);
-			map.put(i + 1, failureRateArray[i]);
+			indexArray[i] = i + 1;
 		}
-		System.out.println("Before sorting : " + map);
-		sortByValue(map);
-		System.out.println("After sorting : " + map);
-		double compareIndex = resultArray[0];
-		double buf;
-		for(int i = 0; i < resultArray.length; i++) {
-			for(int j = 0; j < resultArray.length-i-1;j++) {
-				if(resultArray[i] <= resultArray[i+1]) {
-					buf = resultArray[i];
-					resultArray[i] = resultArray[i+1];
-					resultArray[i+1] = buf;
+		for (int i = 0; i < N; i++) {
+			System.out.print(indexArray[i] + " ");
+			System.out.print(resultArray[i] + " ");
+			System.out.println();
+		}
+		double d_buf;
+		int i_buf;
+		for (int i = 0; i < resultArray.length; i++) {
+			for (int j = 0; j < resultArray.length - i - 1; j++) {
+				if (i == j) {
+					continue;
+				}
+				if (resultArray[j] >= resultArray[j + 1]) {
+					d_buf = resultArray[j + 1];
+					i_buf = indexArray[j + 1];
+					resultArray[j + 1] = resultArray[j];
+					indexArray[j + 1] = indexArray[j];
+					resultArray[j] = d_buf;
+					indexArray[j] = i_buf;
 				}
 			}
 		}
-		for(double item : resultArray) {
-			System.out.print(item + " ");
+		for (int i = 0; i < N; i++) {
+			System.out.print(indexArray[i] + " ");
+			System.out.print(resultArray[i] + " ");
+			System.out.println();
 		}
-		
-		
-		
-		
-		
+
+		for (int i = N; i > 0; i--) {
+			answer[N-i] = indexArray[i-1];
+		}
+
 		return answer;
-	}
-
-	private static <K, V> Map<K, V> sortByValue(Map<K, V> map) {
-		List<Entry<K, V>> list = new LinkedList<>(map.entrySet());
-		Collections.sort(list, new Comparator<Object>() {
-			@SuppressWarnings("unchecked")
-			public int compare(Object o1, Object o2) {
-				return ((Comparable<V>) ((Map.Entry<K, V>) (o1)).getValue())
-						.compareTo(((Map.Entry<K, V>) (o2)).getValue());
-			}
-		});
-
-		Map<K, V> result = new LinkedHashMap<>();
-		for (Iterator<Entry<K, V>> it = list.iterator(); it.hasNext();) {
-			Map.Entry<K, V> entry = (Map.Entry<K, V>) it.next();
-			result.put(entry.getKey(), entry.getValue());
-		}
-
-		return result;
 	}
 }
