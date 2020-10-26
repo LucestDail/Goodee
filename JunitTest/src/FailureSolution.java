358a3061237e552116ef7ebb5dfbe36e6bf6409a
hyun <dhtmdgussksm@naver.com>
2020-10-26 12:54:22 +0900

    Update java source
    bia programmers

--- JunitTest/src/FailureSolution.java
+++ JunitTest/src/FailureSolution.java
@@ -18,11 +18,54 @@ class FailureSolution {
 			servivor -= count;
 		}
 
+		Map<Integer, Double> map = new LinkedHashMap<>();
 		double[] resultArray = new double[N];
 		for (int i = 0; i < resultArray.length; i++) {
 			resultArray[i] = failureRateArray[i];
 			System.out.println(resultArray[i]);
+			map.put(i + 1, failureRateArray[i]);
 		}
+		System.out.println("Before sorting : " + map);
+		sortByValue(map);
+		System.out.println("After sorting : " + map);
+		double compareIndex = resultArray[0];
+		double buf;
+		for(int i = 0; i < resultArray.length; i++) {
+			for(int j = 0; j < resultArray.length-i-1;j++) {
+				if(resultArray[i] <= resultArray[i+1]) {
+					buf = resultArray[i];
+					resultArray[i] = resultArray[i+1];
+					resultArray[i+1] = buf;
+				}
+			}
+		}
+		for(double item : resultArray) {
+			System.out.print(item + " ");
+		}
+		
+		
+		
+		
+		
 		return answer;
+	}
+
+	private static <K, V> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Entry<K, V>> list = new LinkedList<>(map.entrySet());
+		Collections.sort(list, new Comparator<Object>() {
+			@SuppressWarnings("unchecked")
+			public int compare(Object o1, Object o2) {
+				return ((Comparable<V>) ((Map.Entry<K, V>) (o1)).getValue())
+						.compareTo(((Map.Entry<K, V>) (o2)).getValue());
+			}
+		});
+
+		Map<K, V> result = new LinkedHashMap<>();
+		for (Iterator<Entry<K, V>> it = list.iterator(); it.hasNext();) {
+			Map.Entry<K, V> entry = (Map.Entry<K, V>) it.next();
+			result.put(entry.getKey(), entry.getValue());
+		}
+
+		return result;
 	}
 }
