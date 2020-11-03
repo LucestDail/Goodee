9f92668b1da4ee0577119de382093c5aae392f06
hyun <dhtmdgussksm@naver.com>
2020-11-03 10:30:42 +0900

    Update source code
    change some description about code and korean comment

--- chap18/src/Test1102/Test1.java
+++ chap18/src/Test1102/Test1.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Employee {
-	private String name; // 이름
-	private String work; // 업무
-	private String position; // 직급
-	private int age; // 나이
+	private String name;
+	private String work;
+	private String position;
+	private int age;
 
 	public Employee(String name, String work, String position, int age) {
 		this.name = name;
@@ -54,16 +54,11 @@ public class Test1 {
 		
 		Stream<Employee> st = null;
 		CONST c = new CONST();
-		
-		// 1. emp.txt 파일을 읽어서, st 객체에 저장
 		st = StreamMaker();
-		
-		// 2. 사원들이 속한 직급 정보 출력하기
-		System.out.println("=== 사원들이 속한 직급 정보 ===");
+		System.out.println("===Position List===");
 		st.map(stObj -> stObj.getPosition()).distinct().forEach(stObj -> System.out.print(stObj + " "));
 		System.out.println();
-		// 3. 업무별 이름 출력하기
-		System.out.println("=== 업무별 이름 출력하기 ===");
+		System.out.println("===Grouping by Job List===");
 		st = StreamMaker();
 		st.collect(Collectors.groupingBy(s->s.getWork())).forEach((k,v) -> {
 			System.out.print(k + " : ");
@@ -72,8 +67,7 @@ public class Test1 {
 			}
 			System.out.println();
 		});
-		// 4. 직급별 나이의 평균 출력하기
-		System.out.println("=== 직급별 나이의 평균 출력하기 ===");
+		System.out.println("===Average Position Age===");
 		st = StreamMaker();
 		st.collect(Collectors.groupingBy(s->s.getPosition())).forEach((k,v) -> {
 			System.out.print(k + " : ");
--- chap18/src/Test1102/Test2.java
+++ chap18/src/Test1102/Test2.java
@@ -7,19 +7,18 @@ import java.util.*;
 public class Test2 {
 	public static void main(String[] args){
 		StringBuffer sb = new StringBuffer();
-		System.out.println("읽으려는 파일 명을 입력하세요(확장자 생략)");
+		System.out.println("input your file name");
 		Scanner scan = new Scanner(System.in);
 		String targetFile = scan.next();
-		sb.append(targetFile + ".txt");
 		try {
-			FileInputStream fis = new FileInputStream(sb.toString());
+			FileInputStream fis = new FileInputStream(targetFile);
 			byte[] buf = new byte[fis.available()];
 			int data = 0;
 			while ((data = fis.read(buf)) != -1) {
 				System.out.print(new String(buf, 0, data));
 			}
 		} catch (IOException e) {
-			System.out.println("해당 파일 없음");
+			System.out.println("file is not existed");
 			return;
 		}
 	}
