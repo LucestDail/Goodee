9b2a81afe739765bd9e3dc2e2398d19fa861f01c
hyun <dhtmdgussksm@naver.com>
2020-11-06 10:25:40 +0900

    Change some java source code
    bia goodee

---
+++ chap19/src/OnlineBaseballGame/BaseBallServerEx.java
@@ -0,0 +1,116 @@
+package OnlineBaseballGame;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class BaseBallServerEx {
+	static List<PrintWriter> outs = new ArrayList<>();
+	static List<BaseBallThread> threads = new ArrayList<>();
+	static List<Integer> arr = numberSelect();
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(44444);
+		while (true) {
+			System.out.println("Baseball Game Waiting for Client...");
+			Socket client = server.accept();
+			System.out.println("Client : " + client.getInetAddress());
+			outs.add(new PrintWriter(client.getOutputStream()));
+			BaseBallThread bt = new BaseBallThread(client, arr);
+			threads.add(bt);
+			bt.start();
+		}
+	}
+
+	static List<Integer> numberSelect() {
+		LinkedHashSet<Integer> answer = new LinkedHashSet<Integer>();
+		int data;
+		while (answer.size() < 4) {
+			data = (int) (Math.random() * 10);
+			answer.add(data);
+		}
+		System.out.println(answer);
+		return new ArrayList<Integer>(answer);
+	}
+}
+
+class BaseBallThread extends Thread {
+	Socket client;
+	List<Integer> arr;
+	BufferedReader br;
+	PrintWriter pw;
+	String number;
+	boolean able = true;
+
+	public BaseBallThread(Socket client, List<Integer> arr) {
+		// TODO Auto-generated constructor stub
+		this.client = client;
+		this.arr = arr;
+		try {
+			br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			pw = new PrintWriter(client.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (able) {
+			try {
+				number = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			char[] carr = number.toCharArray();
+			int rcvData[] = new int[4];
+			int strike = 0;
+			int ball = 0;
+			int i, j;
+			for (i = 0; i < rcvData.length; i++) {
+				for (j = 0; j < rcvData.length; j++) {
+					if (rcvData[i] == arr.get(j)) {
+						if (i == j) {
+							strike++;
+						} else {
+							ball++;
+						}
+					}
+				}
+			}
+			if (strike == 4) {
+				pw.println(number + " is correct. game end");
+				pw.flush();
+				allClientClose();
+				break;
+			} else {
+				pw.println(number + " : " + strike + " strike, " + ball + " ball");
+				pw.flush();
+			}
+		}
+	}
+
+	private void allClientClose() {
+		Iterator<PrintWriter> list = BaseBallServerEx.outs.iterator();
+		BaseBallServerEx.arr = BaseBallServerEx.numberSelect();
+		while (list.hasNext()) {
+			PrintWriter pw = list.next();
+			pw.println("another use has finished. game end");
+			pw.flush();
+			list.remove();
+		}
+		BaseBallServerEx.outs.clear();
+		for (BaseBallThread t : BaseBallServerEx.threads) {
+			t.able = false;
+			BaseBallServerEx.threads.clear();
+		}
+
+	}
+
+}
--- chap19/src/OnlineBaseballGame/Client.java
+++ chap19/src/OnlineBaseballGame/Client.java
@@ -5,50 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-
-//		System.out.println("entering your access ip address(000.000.000.000 or localhost)");
-//		Scanner scan = new Scanner(System.in);
-//		String ip = scan.nextLine();
-//		System.out.println("entering your access port(8000 or anything else)");
-//		int port = scan.nextInt();
-//		scan.close();
-		String ip = "localhost";
-		int port = 8000;
-		Socket client = null;
-		PrintWriter pw = null;
-		BufferedReader br = null;
-		try {
-			String input;
-			BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-			client = new Socket(ip, port);
-			br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			System.out.println(br.readLine());
-			System.out.println(br.readLine());
-			pw = new PrintWriter(client.getOutputStream());
-			while ((input = stdin.readLine()) != null) {
-				if (input.equals("bye")) {
-					System.out.println("Áßµµ Æ÷±â!");
-					break;
-				}
-				pw.println(input);
-				pw.flush();
-				System.out.println(br.readLine());
-				System.out.println(br.readLine());
+		BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("entering your access ip address(000.000.000.000 or localhost)");
+		String ip = sysin.readLine();
+		System.out.println("entering your access port(8000 or anything else)");
+		int port = sysin.read();
+		Socket client = new Socket(ip, port);
+		BufferedReader sbr = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		PrintWriter pw = new PrintWriter(client.getOutputStream());
+		while (true) {
+			System.out.println("4 number which is different one");
+			String number = sysin.readLine();
+			pw.println(number);
+			pw.flush();
+			String data = sbr.readLine();
+			System.out.println(data);
+			if (data.indexOf("ì¢…ë£Œ") >= 0) {
+				break;
 			}
-			pw.close();
-			br.close();
-			stdin.close();
-			client.close();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-
+		sysin.close();
+		sbr.close();
+		pw.close();
+		client.close();
 	}
-
 }
