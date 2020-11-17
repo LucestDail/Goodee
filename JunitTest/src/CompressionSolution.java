db4255f9248128e77b79d7c7463e57e9fb1351cc
hyun <dhtmdgussksm@naver.com>
2020-11-17 13:43:47 +0900

    Update CompressionSolution.java

--- JunitTest/src/CompressionSolution.java
+++ JunitTest/src/CompressionSolution.java
@@ -3,25 +3,27 @@ import java.util.ArrayList;
 public class CompressionSolution {
 
 	public static int[] solution(String keyword) {
-		int[] returnArray = {};
-		
+		ArrayList<Integer> returnArrayList = new ArrayList<>();
+		// 1. 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
 		ArrayList<Character> charlist = new ArrayList<>();
-		for(int i = 'A'; i < 'Z'; i++) {
+		for(int i = 'A'; i <= 'Z'; i++) {
 			charlist.add((char)i);
 		}
-		
-		for(int i = 0; i < keyword.length(); i++) {
-			if(charlist.contains(keyword.charAt(i))){
-				
-			}
+		for(char item : charlist) {
+			System.out.println(item);
 		}
 		
-		// TODO Auto-generated method stub
-		return returnArray;
-	}
-	private int findIndex() {
 		
-		return 1;
+		
+		
+		
+		
+		// 결과 반환
+		int[] returnArray = new int[returnArrayList.size()];
+		for(int i = 0; i < returnArrayList.size(); i++) {
+			returnArray[i] = returnArrayList.get(i);
+		}
+		return returnArray;
 	}
 
 }
